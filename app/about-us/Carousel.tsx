49f4cfd1f2238683c88ab33111494391bc3a9e0d
Daniel B <daarf92@gmail.com>
2023-11-04 20:58:06 +0000

    carousel moved cards horizontal beyond screen width

--- app/about-us/Carousel.tsx
+++ app/about-us/Carousel.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Image from 'next/image'
 import './carousel.css';
+import { FaChevronLeft, FaChevronRight } from "react-icons/fa6";
 import note from '../../public/imgs/note.svg';
 import edu from '../../public/imgs/edu.svg';
 import aim from '../../public/imgs/aim.svg';
@@ -9,17 +10,17 @@ const Carousel = () => {
   return (
     <div>
       <div className='carousel'>
-          <div className='card' id="music">
+          <div className='car-card' id="music">
             <div className='card-text'>Note warriors is a music education platform. We specifically focus on the music curriculum taught in Irish schools.</div>
-            <div><Image className='card-image' src={note} alt='note icon' /></div>
+            <div><Image className='car-card-image' src={note} alt='note icon' /></div>
           </div>
-          <div className='card' id="experience">
+          <div className='car-card' id="experience">
             <div className='card-text'>Our goal is to make the learning experience fun whilst providing teachers with student progress and tangible results.</div>
-            <div ><Image className='card-image' src={edu} alt="education icon" /></div>
+            <div ><Image className='car-card-image' src={edu} alt="education icon" /></div>
           </div>
-          <div className='card' id="aim">
+          <div className='car-card' id="aim">
             <div className='card-text'> This platform aims to test what students have been learning in the classroom and giving them feedback.</div>
-            <div><Image className='card-image' src={aim} alt='aims icon' /></div>
+            <div><Image className='car-card-image' src={aim} alt='aims icon' /></div>
           </div>
       </div>
     </div>
