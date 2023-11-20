2cc69d69a4c0d0ab77572be12a68cc983644ccf0
Daniel B <daarf92@gmail.com>
2023-11-20 18:10:21 +0000

    faq page started. figure out shading issue

---
+++ app/faq/page.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './faq.css';
+
+const page = () => {
+  return (
+    <div className='faq'>
+      <div className='shader'>
+
+      </div>
+    </div>
+  )
+}
+
+export default page
